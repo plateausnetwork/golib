43cc9742345c9d8c41fd3db76309a304a4ddf7f8
Guilherme C. Passos <guissos@gmail.com>
2022-08-28 20:39:52 -0300

    feat(FM-599): add mongodb

---
+++ mongodb/mongodb_errors.go
@@ -0,0 +1,12 @@
+package mongodb
+
+import "errors"
+
+var (
+	ErrCouldNotConnect = func(err error) error {
+		return errors.New("could not connect to mongodb, details: " + err.Error())
+	}
+	ErrCantPing = func(err error) error {
+		return errors.New("can't ping mongodb, details: " + err.Error())
+	}
+)
